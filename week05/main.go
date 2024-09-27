ba8d3b7955cfb0c7afc22189f0e7e92c71b999b8
김재민[컴퓨터정보공학과] <cvx3449@gmail.com>
2024-09-27 15:14:59 +0900

    13

---
+++ week05/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var i int = 55
+	var f float32 = 3.99
+	i := 55
+	fmt.Println(f)
+	fmt.Printf("i는 %d\n", i)
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
+}
